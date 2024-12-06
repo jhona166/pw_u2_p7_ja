7fe8507cd28146754e66bf497a8a529c3784ae3a
jhona166 <jdaltamiranoo@uce.edu.ec>
2024-12-05 19:37:12 -0500

    Taller15

--- js/appfrases.js
+++ js/appfrases.js
@@ -1,23 +1,70 @@
-const frases = [{frase:'El que madruga Dios le ayuda',autor:'Pepito Perez'},
-    {frase:'El que madruga Dios le ayuda 1',autor:'Pepito Perez'},
-    {frase:'El que madruga Dios le ayuda 2',autor:'Juan Teran'},
-    {frase:'El que madruga Dios le ayuda 3',autor:'Sebastian Blanco'},
-    {frase:'El que madruga Dios le ayuda 4',autor:'Anton González'},
-    {frase:'El que madruga Dios le ayuda 5',autor:'Carlos Lopez'},
-    {frase:'El que madruga Dios le ayuda 6',autor:'Reinaldo Rueda'}]
-// Declarar un objeto de Vue
+const frases = [{ frase: 'El que madruga Dios le ayuda', autor: 'Pepito Perez' },
+{ frase: 'El que madruga Dios le ayuda 1', autor: 'Pepito Perez' },
+{ frase: 'El que madruga Dios le ayuda 2', autor: 'Juan Teran' },
+{ frase: 'El que madruga Dios le ayuda 3', autor: 'Sebastian Blanco' },
+{ frase: 'El que madruga Dios le ayuda 4', autor: 'Anton González' },
+{ frase: 'El que madruga Dios le ayuda 5', autor: 'Carlos Lopez' },
+{ frase: 'El que madruga Dios le ayuda 6', autor: 'Reinaldo Rueda' }]
 const app = Vue.createApp({
-   
- methods: {
-   
- },
- data(){
-    return{
-      listafrases: frases,
+  methods: {
+      agregarFrase() {
+          console.log(this.frase);
+          console.log(this.autor);
 
-    }
-    }
-}) 
+          const nuevaFrase = {
+              frase: this.frase,
+              autor: this.autor
+          }
 
- 
-app.mount('#myApp'); // Montar la aplicación en el div con id myApp
+          this.listafrases.unshift(nuevaFrase);
+      },
+
+      agregarFraseFinal() {
+          console.log(this.frase);
+          console.log(this.autor);
+
+          const nuevaFrase = {
+              frase: this.frase,
+              autor: this.autor
+          }
+
+          this.listafrases.push(nuevaFrase);
+      },
+
+      eventoKeyPress({ charCode, cancelable, key, keyCode, target }) {
+          if (key === 'Enter') {
+              console.log('Evento');
+              //console.log(event);
+              console.log(key);
+              console.log(charCode);
+              console.log(keyCode);
+              console.log(cancelable);
+              console.log(target.baseURI);
+              this.agregarFraseFinal();
+          }
+      },
+
+      eventoKeyPressModificador() {
+          console.log('Evento');
+          console.log(key);
+          console.log(charCode);
+          console.log(keyCode);
+          console.log(cancelable);
+          console.log(target.baseURI);
+          //console.log(event);
+          this.agregarFraseFinal();
+
+      }
+
+  },
+  data() {
+      return {
+          listafrases: frases,
+          frase: null,
+          //autor: 'Sin autor'
+          autor: null
+      }
+  }
+})
+
+app.mount('#myApp')
